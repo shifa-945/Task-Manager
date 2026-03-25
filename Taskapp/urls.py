64aaf56b4f98ffd50787e36bf80513c0b24a0e31
shifa-945 <shifanasrin319@gmail.com>
2026-03-25 18:41:31 +0530

    Updated project code

--- Taskapp/urls.py
+++ Taskapp/urls.py
@@ -5,6 +5,10 @@ from Taskapp import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('register/',views.register,name='register'),
+    path('login/',views.User_login,name='login'),
+    path('after_login/',views.after_login,name='after_login'),
+    path('logout/',views.User_logout,name='logout'),
     path('Task_add/', views.Task_add, name='Task_add'),
     path('Task_list/', views.Task_list, name='Task_list'),
     path('toggle/<int:id>/',views.toggle_task,name='toggle'),
--- Taskapp/views.py
+++ Taskapp/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import redirect, render
 
-from Taskapp.forms import TaskForm
+from Taskapp.forms import TaskForm,UserRegistrationForm
 from Taskapp.models import Task
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
+def register(request):
+    if request.method=='POST':
+        form=UserRegistrationForm(request.POST)
+        form.is_valid()
+        user=form.save()
+        login(request,user)
+        return redirect('home')
+    else:
+        form=UserRegistrationForm()
 
+    return render(request,'register.html',{'form':form})
+
+def User_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('after_login')
+    return render(request,'login.html')
+
+def after_login(request):
+    return render(request,'after_login.html')
+
+def User_logout(request):
+     logout(request)
+     return redirect('home')     
+        
+@login_required
 def Task_add(request):
     if request.method=='POST':
         form = TaskForm(request.POST)
@@ -21,6 +54,7 @@ def Task_add(request):
     return render(request,'Task_add.html', {'form': form})
 
 
+@login_required
 def Task_list(request):
     tasks=Task.objects.all()
     return render(request,'Task_list.html',{'tasks': tasks})
@@ -31,6 +65,7 @@ def toggle_task(request,id):
     task.save()
     return redirect('Task_list')
 
+@login_required
 def Task_delete(request, id):
     task = Task.objects.get(id=id)
     task.delete()
